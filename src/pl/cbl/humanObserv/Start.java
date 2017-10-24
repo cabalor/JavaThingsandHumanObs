e7bb8513acb402c037066a8339bec3a7b3832112
cabalor <cabalor@gmail.com>
2017-10-24 23:29:03 +0200

    meta

--- src/pl/cbl/humanObserv/Start.java
+++ src/pl/cbl/humanObserv/Start.java
@@ -54,32 +54,29 @@ public class Start {
 	}
 
 	public static void game(List<Human> players, Scanner scn) {
-		System.out.println("how many rounds will we drink?");
+		System.out.println("how many rounds will you drink?");
 		int limit = scn.nextInt();
+		scn.nextLine();
 		boolean gameOn = true;
 		DrinkFactory df = new DrinkFactory();
 		String s;
 		while (gameOn == true) {
 			for (int i = 0; i < limit; i++) {
+				for (int j = 0; j < players.size() * limit; j++) {
+					System.out.println("Round " + i);
 				System.out.println(
-						players.get(i).getName() + " choose drink from list: V for Vodka, B for beer, W for wine");
+						players.get(j).getName() + " choose drink from list: V for Vodka, B for beer, W for wine");
 				s = scn.nextLine();
 				System.out.println(s);
 				Drink dr = df.makeDrink(s);
 				System.out.println(dr.Toxicity);
-				players.get(i).setToxicity(dr.getToxicity());
-				if(dr.getToxicity()>70) {
+				players.get(j).setToxicity(dr.getToxicity());
+				if (dr.getToxicity() > 70) {
 					System.out.println("chlejesz metanol");
 				} else {
-				System.out.println(players.get(i).getName()+ " toksycznosc "+ players.get(i).getToxicity());
+					System.out.println(players.get(j).getName() + " toksycznosc " + players.get(i).getToxicity());
 				}
-				// dr = df.makeDrink(s);
-				// System.out.println(dr.toString());
-
-				// players.get(i).setToxicity(dr.getToxicity());
-
-				// players.get(i).setToxicity(dr.getToxicity());
-				// System.out.println(players.get(i).getToxicity());
+				}
 			}
 
 		}
