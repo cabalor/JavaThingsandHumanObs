a11d50eef6510c16d1c47ea500aeae128172f99a
cabalor <cabalor@gmail.com>
2017-10-31 22:25:40 +0100

    re

--- src/pl/cbl/humanObserv/Human.java
+++ src/pl/cbl/humanObserv/Human.java
@@ -2,14 +2,14 @@ package pl.cbl.humanObserv;
 
 import java.util.Random;
 
-public class Human implements HumanObs{
+public class Human implements HumanObs {
 
 	private String name;
 	private double health = 100;
 	private double toxicity = 0;
-	
+
 	DrinkFactory df = new DrinkFactory();
-	
+
 	public String getName() {
 		return name;
 	}
@@ -25,7 +25,7 @@ public class Human implements HumanObs{
 	public void setHealth(double health) {
 		this.health = health;
 	}
-	
+
 	public double getToxicity() {
 		return toxicity;
 	}
@@ -34,54 +34,51 @@ public class Human implements HumanObs{
 		this.toxicity = toxicity;
 	}
 
-	public Human() {}
-	
+	public Human() {
+	}
+
 	public Human(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Human [name=" + name + ", health=" + health + ", toxicity=" + toxicity +"]";
+		return "Human [name=" + name + ", health=" + health + ", toxicity=" + toxicity + "]";
 	}
 
 	@Override
 	public void downHealth(double health) {
 		Random random = new Random();
-		 health = health - (double)random.nextInt(5);
-		 this.setHealth(health);
-		 System.out.println("health value down "+health + " "+this.getName());
-		
+		health = health - (double) random.nextInt(5);
+		this.setHealth(health);
+		System.out.println("health value down " + health + " " + this.getName());
+
 	}
-	@Override 
+
+	@Override
 	public void upHealth(double health) {
 		Random random = new Random();
-		 health = health + (double)random.nextInt(5);
-		 this.setHealth(health);
-		 System.out.println("health value up "+health+" "+this.getName());
-		
+		health = health + (double) random.nextInt(5);
+		this.setHealth(health);
+		System.out.println("health value up " + health + " " + this.getName());
+
 	}
 
 	@Override
 	public void drink(double toxi) {
 		Random rnd = new Random();
-		int x = rnd.nextInt(9)+1;
+		int x = rnd.nextInt(9) + 1;
 		Drink dr = new Drink();
-		if(x<=3) {
-		dr = df.makeDrink("Vodka");}
-		else if (x>3 && x<=6){
+		if (x <= 3) {
+			dr = df.makeDrink("Vodka");
+		} else if (x > 3 && x <= 6) {
 			dr = df.makeDrink("Wine");
-		} else if(x>6 && x<=9) {
+		} else if (x > 6 && x <= 9) {
 			dr = df.makeDrink("Beer");
 		}
 		toxi = toxi + dr.getToxicity();
 		this.setToxicity(toxi);
-		System.out.println("toxicity goes up for " + dr.getToxicity()+"\n toxi is now "+ toxi+"\n-----------");
+		System.out.println("toxicity goes up for " + dr.getToxicity() + "\n toxi is now " + toxi + "\n-----------");
 	}
-	
-	
-	
-	
-	
-	
+
 }
--- src/pl/cbl/humanObserv/Wine.java
+++ src/pl/cbl/humanObserv/Wine.java
@@ -1,35 +1,25 @@
 package pl.cbl.humanObserv;
 
-public class Wine extends Drink{
+public class Wine extends Drink {
 
-	
 	Wine(double Toxi) {
 		super(Toxi);
 		// TODO Auto-generated constructor stub
 	}
 
-
-	private double Toxi ;
-	
-	
-	
-
+	private double Toxi;
 
 	public double getToxi() {
 		return Toxi;
 	}
 
-
 	public void setToxi(double toxi) {
 		Toxi = toxi;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Wine [Toxi=" + Toxi + "]";
 	}
 
-	
-	
 }
