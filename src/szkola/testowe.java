770e3b7cd668d69d9b88688fa3e9f2c828a6124e
cabalor <cabalor@gmail.com>
2017-12-29 18:10:44 +0100

    asdada

---
+++ src/szkola/testowe.java
@@ -0,0 +1,103 @@
+package pl.net.bluesoft.testranger.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class testowe {
+
+    public static void main(String[] args){
+
+        new Thread(()-> {
+            System.out.println("dupa dupa");
+            System.out.println("dsdd");
+            System.out.println("asdsadad");
+        }).start();
+
+
+        emp jon = new emp("dupa", 20);
+        emp jan = new emp("sds", 40);
+
+
+
+        List<emp> emps = new ArrayList<>();
+        emps.add(jon);
+        emps.add(jan);
+
+
+//        Collections.sort(emps, new Comparator<emp>() {
+//            @Override
+//            public int compare(emp emp1, emp emp2) {
+//                return emp1.getName().compareTo(emp2.getName());
+//            }
+//        });
+
+        Collections.sort(emps, ( emp1, emp2) ->
+        emp1.getName().compareTo(emp2.getName()));
+
+
+        for(emp e: emps){
+            System.out.println(e);
+        }
+
+//        String sill = doString(new UpConcat() {
+//            @Override
+//            public String upAndConcat(String s1, String s2) {
+//                return s1.toUpperCase()+s2.toUpperCase();
+//            }
+//        },
+//        emps.get(0).getName(), emps.get(1).getName());
+//        System.out.println(sill);
+
+
+        UpConcat uc = (s1, s2) -> s1.toUpperCase()+s2.toUpperCase();
+
+        String sill = doString(uc, emps.get(0).getName(), emps.get(1).getName());
+
+    }
+
+
+    public final static String doString(UpConcat uc, String s1, String s2){
+        return uc.upAndConcat(s1, s2);
+    }
+
+}
+
+
+class emp{
+
+    private String name;
+    private int age;
+
+
+    emp(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setInt(int age){
+        this.age = age;
+    }
+
+    public String toString(){
+        return "name "+name+" wiek "+ age;
+    }
+
+}
+
+
+interface UpConcat{
+    public String upAndConcat(String s1, String s2);
+}
