e36dd6b4d56386d61efd2f0a6f1df31dabc5277b
cabalor <cabalor@gmail.com>
2018-01-10 19:44:00 +0100

    asd

--- src/szkola/NoweLambdy.java
+++ src/szkola/NoweLambdy.java
@@ -3,8 +3,11 @@ package szkola;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -83,8 +86,34 @@ public class NoweLambdy {
 	        	return worker.getName().substring(0, worker.getName().indexOf(' '));
 	        };
 	        
+	        Random rnd1 = new Random();
+	        for(Workery w: work) {
+	        	if(rnd1.nextBoolean()) {
+	        		System.out.println(dawajImie(imie, w));
+	        	} else {
+	        		System.out.println(dawajImie(nazwa, w));
+	        	}
+	        }
+	        
+	        Function<Workery, String> doGory = worker -> worker.getName().toUpperCase();
+	        Function<String, String> dodanie = name -> name.substring(0, name.indexOf(' '));
+	        Function chaindeFunc = doGory.andThen(dodanie);
+	        System.out.println(chaindeFunc.apply(work.get(0)));
+	        
+	        BiFunction<String, Workery, String> conAge = (String name, Workery w) -> {
+	        	return name.concat( " " + w.getAge());
+	        };
+	        
+	        String upName = doGory.apply(work.get(0));
+	        System.out.println(conAge.apply(upName, work.get(0)));
 	        
 	        
+	        IntUnaryOperator in5 = i -> i+ 5;
+	        System.out.println(in5.applyAsInt(5));
+	        
+	        Consumer<String> c1 = s -> s.toUpperCase();
+	        Consumer<String> c2 = s -> System.out.println(s);
+	        c1.andThen(c2).accept("dupa dupa");
 	        
 //	        work.forEach(wo -> {
 //	        	if(wo.getAge() > 30) {
@@ -102,6 +131,11 @@ public class NoweLambdy {
 
 	}
 
+	private static String dawajImie(Function<Workery, String> imie, Workery work) {
+		return imie.apply(work);
+	}
+	
+	
 	private static void printWorkeryAge(List<Workery> work, String ageText, Predicate<Workery> warunekWieku) {
 		System.out.println(ageText);
 		for(Workery wo: work) {
