591d25f7c0a2f76c220a35e24eaa6cd985fc5521
cabalor <cabalor@gmail.com>
2018-01-25 20:59:13 +0100

    siedzenia

---
+++ src/col/Dom.java
@@ -0,0 +1,22 @@
+package col;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dom {
+
+	private final String name;
+	private List<Siedzenie> siedzenia = new ArrayList<>();
+	
+	public Dom(String name, int row, int kPerRow) {
+		this.name = name;
+	
+	
+	int lastRow = 'A' + (row -1);
+	for(char r = 'A'; r <=lastRow; r++) {
+		
+	}
+	
+	}
+	
+}
---
+++ src/col/Start.java
@@ -0,0 +1,14 @@
+package col;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+	
+	
+}
